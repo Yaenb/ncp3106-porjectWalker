b27542c27d30ae3f4b3ab76879ea22626f5c12e0
Angel Grace Ramirez <mochiiowo4@gmail.com>
2025-11-07 00:18:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,16 @@
-<?php
-    include('include/auth_check.php');
-    include('include/header.php');
+<?php 
+include('include/auth_check.php'); 
+include('include/header.php'); 
 ?>
-
 <div id="map"></div>
 
 <div id="bottom-bar">
-    <input
-        type="text"
-        id="searchLocation"
-        placeholder="Search location..."
-    >
-    <button id="walkBtn">Walk</button>
+  <input type="text" id="searchLocation" placeholder="Search location...">
+  <button id="walkBtn">Walk</button>
 </div>
 
 <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 <script src="assets/js/map.js"></script>
 <script src="assets/js/rating.js"></script>
 <script src="assets/js/api.js"></script>
-
-<?php
-    include('include/footer.php');
-?>
+<?php include('include/footer.php'); ?>
